2f95d0a258de154394c8dae10e7d23b909734ec0
Alexey Shumkin <Alex.Crezoff@gmail.com>
2023-12-14 19:04:19 +0300

    feat(ci view): add `--mr [MR]` opt to show pipeline for a specific MR
    It is convenient to see existing pipelines for an arbitrary MR but not
    to switch a current Git branch.

--- internal/commands/ci/view/view.go
+++ internal/commands/ci/view/view.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"runtime/debug"
+	"strconv"
 	"strings"
 	"time"
 	"unicode/utf8"
@@ -26,6 +27,7 @@ import (
 	"gitlab.com/gitlab-org/cli/internal/api"
 	"gitlab.com/gitlab-org/cli/internal/cmdutils"
 	"gitlab.com/gitlab-org/cli/internal/commands/ci/ciutils"
+	"gitlab.com/gitlab-org/cli/internal/commands/mr/mrutils"
 	"gitlab.com/gitlab-org/cli/internal/config"
 	"gitlab.com/gitlab-org/cli/internal/git"
 	"gitlab.com/gitlab-org/cli/internal/glrepo"
@@ -36,6 +38,7 @@ import (
 
 type options struct {
 	io           *iostreams.IOStreams
+	factory      cmdutils.Factory
 	gitlabClient func() (*gitlab.Client, error)
 	baseRepo     func() (glrepo.Interface, error)
 	config       func() config.Config
@@ -43,6 +46,7 @@ type options struct {
 	refName       string
 	openInBrowser bool
 	pipelineID    int64
+	forMR         bool
 }
 
 type ViewJobKind int64
@@ -103,6 +107,7 @@ func ViewJobFromJob(job *gitlab.Job) *ViewJob {
 
 func NewCmdView(f cmdutils.Factory) *cobra.Command {
 	opts := options{
+		factory:      f, // quick fix
 		io:           f.IO(),
 		gitlabClient: f.GitLabClient,
 		baseRepo:     f.BaseRepo,
@@ -148,12 +153,13 @@ func NewCmdView(f cmdutils.Factory) *cobra.Command {
 				return err
 			}
 
-			return opts.run(cmd.Context())
+			return opts.run(cmd.Context(), args)
 		},
 	}
 
 	pipelineCIView.Flags().
 		StringVarP(&opts.refName, "branch", "b", "", "Check pipeline status for a branch or tag. Defaults to the current branch.")
+	pipelineCIView.Flags().BoolVarP(&opts.forMR, "mr", "m", false, "Check pipeline status for a MR. (Default is the current MR)")
 	pipelineCIView.Flags().BoolVarP(&opts.openInBrowser, "web", "w", false, "Open pipeline in a browser. Uses default browser, or browser specified in BROWSER variable.")
 	pipelineCIView.Flags().Int64VarP(&opts.pipelineID, "pipelineid", "p", 0, "Check pipeline status for a specific pipeline ID.")
 	pipelineCIView.MarkFlagsMutuallyExclusive("branch", "pipelineid")
@@ -177,7 +183,7 @@ func (o *options) complete(args []string) error {
 	return nil
 }
 
-func (o *options) run(ctx context.Context) error {
+func (o *options) run(ctx context.Context, args []string) error {
 	// Check if we're in an interactive terminal (TTY required for TUI)
 	if !o.io.IsOutputTTY() {
 		return fmt.Errorf("ci view requires an interactive terminal (TTY).\nFor non-interactive use, try:\n  - 'glab ci status' to check pipeline status\n  - 'glab ci get' to view pipeline details\n  - 'glab ci trace' to view job logs")
@@ -200,7 +206,41 @@ func (o *options) run(ctx context.Context) error {
 	var commit *gitlab.Commit
 	var commitSHA string
 
-	if o.pipelineID != 0 {
+	if o.forMR {
+		var mrIDStr string
+		var mrID int64
+		if len(args) > 0 {
+			mrIDStr = args[0]
+		}
+		if len(mrIDStr) > 0 {
+			mrID, err = strconv.ParseInt(mrIDStr, 10, 0)
+			if err != nil {
+				return fmt.Errorf("MR ID id not an integer: %s (%w)", mrIDStr, err)
+			}
+		}
+		if mrID == 0 {
+			mr, _, err := mrutils.MRFromArgs(ctx, o.factory, args, "any")
+			if err != nil {
+				return err
+			}
+			mrID = mr.IID
+		}
+		pipeInfos, _, err := client.MergeRequests.ListMergeRequestPipelines(repo.FullName(), mrID)
+		if err != nil {
+			return err
+		}
+		if len(pipeInfos) == 0 {
+			return fmt.Errorf("Cannot find pipelines for MR %d", mrID)
+		}
+		commit = &gitlab.Commit{
+			ID:           pipeInfos[0].SHA,
+			LastPipeline: pipeInfos[0],
+		}
+		commitSHA = commit.ID
+		pipelineID = commit.LastPipeline.ID
+		webURL = pipeInfos[0].WebURL
+		pipelineCreatedAt = *pipeInfos[0].CreatedAt
+	} else if o.pipelineID > 0 {
 		pipeline, _, err := client.Pipelines.GetPipeline(projectID, o.pipelineID)
 		if err != nil {
 			return err
@@ -412,36 +452,48 @@ func inputCapture(
 				break
 			}
 			modalVisible = true
-			modal := tview.NewModal().
-				SetBackgroundColor(tcell.ColorDefault).
-				SetText(fmt.Sprintf("Are you sure you want to run %s?", curJob.Name)).
-				AddButtons([]string{"✘ No", "✔ Yes"}).
-				SetDoneFunc(func(buttonIndex int, buttonLabel string) {
-					modalVisible = false
-					root.RemovePage("yesno")
-					if buttonLabel != "✔ Yes" {
+			if curJob.Status == "created" {
+				modal := tview.NewModal().
+					SetText(fmt.Sprintf("The job %s is not retriable", curJob.Name)).
+					AddButtons([]string{"OK"}).
+					SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+						modalVisible = false
+						root.RemovePage("ok")
 						app.ForceDraw()
-						return
-					}
-					root.RemovePage("logs-" + curJob.Name)
-					app.ForceDraw()
+					})
+				root.AddAndSwitchToPage("ok", modal, false)
+			} else {
+				modal := tview.NewModal().
+					SetBackgroundColor(tcell.ColorDefault).
+					SetText(fmt.Sprintf("Are you sure you want to run %s?", curJob.Name)).
+					AddButtons([]string{"✘ No", "✔ Yes"}).
+					SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+						modalVisible = false
+						root.RemovePage("yesno")
+						if buttonLabel != "✔ Yes" {
+							app.ForceDraw()
+							return
+						}
+						root.RemovePage("logs-" + curJob.Name)
+						app.ForceDraw()
 
-					job, err := api.PlayOrRetryJobs(
-						apiClient,
-						projectID,
-						curJob.ID,
-						curJob.Status,
-					)
-					if err != nil {
-						app.Stop()
-						log.Fatal(err)
-					}
-					if job != nil {
-						curJob = ViewJobFromJob(job)
-						app.ForceDraw()
-					}
-				})
-			root.AddAndSwitchToPage("yesno", modal, false)
+						job, err := api.PlayOrRetryJobs(
+							apiClient,
+							projectID,
+							curJob.ID,
+							curJob.Status,
+						)
+						if err != nil {
+							app.Stop()
+							log.Fatal(err)
+						}
+						if job != nil {
+							curJob = ViewJobFromJob(job)
+							app.ForceDraw()
+						}
+					})
+				root.AddAndSwitchToPage("yesno", modal, false)
+			}
 			inputCh <- struct{}{}
 			app.ForceDraw()
 			return nil
