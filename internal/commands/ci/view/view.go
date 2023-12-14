94639aa46f6ecf1c3cfc5e722cdb0b6f6880bc9c
Alexey Shumkin <Alex.Crezoff@gmail.com>
2023-12-14 12:20:15 +0300

    fix(ci view): failure when showing a failed due to YAML error pipeline
    Some pipelines may fail due to .gitlab-ci.yml YAML error. Then `ci view`
    command fails to open such pipeline with no intelligible error shown.
    
    Fix this. Show error message.

--- internal/commands/ci/view/view.go
+++ internal/commands/ci/view/view.go
@@ -707,7 +707,9 @@ func jobsView(
 			b.SetTextAlign(tview.AlignCenter)
 		}
 	}
-	lastStage = jobs[0].Stage
+	if len(jobs) > 0 {
+		lastStage = jobs[0].Stage
+	}
 	rowIdx = 0
 	stageIdx = 0
 	for _, j := range jobs {
@@ -838,7 +840,11 @@ func updateJobs(
 		)
 		if err != nil {
 			app.Stop()
-			log.Fatal(errors.Wrap(err, "failed to find CI jobs."))
+			if err != nil {
+				log.Fatal(errors.Wrap(err, "failed to find CI jobs."))
+			} else {
+				log.Fatal("failed to find CI jobs")
+			}
 		}
 		if len(jobs) == 0 && len(bridges) == 0 {
 			app.Stop()
