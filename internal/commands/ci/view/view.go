720bad288c2ac0bbe26e71f5aa6969706fee2fcb
Alexey Shumkin <Alex.Crezoff@gmail.com>
2023-12-18 13:08:08 +0300

    feat(ci view): add Ctrl+L shortcut to redraw screen
    Sometimes some background jobs run in the same shell session before glab
    run print its messages over glab's view. So, it would be nice to have an
    ability to redraw a screen (Ctrl+L shortcut) for such cases.

--- internal/commands/ci/view/view.go
+++ internal/commands/ci/view/view.go
@@ -123,6 +123,7 @@ func NewCmdView(f cmdutils.Factory) *cobra.Command {
 		- 'Ctrl+D' to cancel a job. If the selected job isn't running or pending, quits the CI/CD view.
 		- 'Ctrl+Q' to quit the CI/CD view.
 		- 'Ctrl+Space' to suspend application and view the logs. Similar to 'glab pipeline ci trace'.
+		- 'Ctrl+L' to redraw the screen.
 		Supports vi style bindings and arrow keys for navigating jobs and logs.
 	`),
 		Example: heredoc.Doc(`
@@ -367,6 +368,12 @@ func inputCapture(
 				app.ForceDraw()
 				return nil
 			}
+		case tcell.KeyCtrlL:
+			if modalVisible || curJob.Kind != Job {
+				break
+			}
+			app.Sync()
+			return nil
 		case tcell.KeyCtrlP, tcell.KeyCtrlR:
 			if modalVisible || curJob.Kind != Job {
 				break
