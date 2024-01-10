40af4749875a9b28ada6699ceb007989cfcced99
Alexey Shumkin <Alex.Crezoff@gmail.com>
2024-01-10 18:19:36 +0300

    fix(ci view): job is still cancelled even when ESC pressed on confirmation
    When a user tries to cancel a job a confirmation dialog is shown. If the
    user presses ESC to interrupt a further process the dialog is closed but
    the job is still canceled. That is not an expected behaviour. Usually,
    canceling a dialog is equivalent to "No" answer.
    
    Fix that.

--- commands/ci/view/view.go
+++ commands/ci/view/view.go
@@ -351,7 +351,7 @@ func inputCapture(
 					SetDoneFunc(func(buttonIndex int, buttonLabel string) {
 						modalVisible = false
 						root.RemovePage("yesno")
-						if buttonLabel == "✘ No" {
+						if buttonLabel != "✔ Yes" {
 							app.ForceDraw()
 							return
 						}
