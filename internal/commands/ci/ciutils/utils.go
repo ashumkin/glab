2e4e244cc2ebdccdbd207821149ea2d37b430a4c
Alexey Shumkin <Alex.Crezoff@gmail.com>
2024-01-29 20:17:07 +0300

    fix(ci view) | skipped job is being waited

--- internal/commands/ci/ciutils/utils.go
+++ internal/commands/ci/ciutils/utils.go
@@ -201,7 +201,7 @@ func runTrace(ctx context.Context, apiClient *gitlab.Client, w io.Writer, pid an
 			return errors.Wrap(err, "failed to find job")
 		}
 		switch job.Status {
-		case "pending":
+		case "pending", "created":
 			fmt.Fprintf(w, "%s is pending... waiting for job to start.\n", job.Name)
 			continue
 		case "manual":
@@ -209,6 +209,7 @@ func runTrace(ctx context.Context, apiClient *gitlab.Client, w io.Writer, pid an
 			continue
 		case "skipped":
 			fmt.Fprintf(w, "%s has been skipped.\n", job.Name)
+			return nil
 		}
 		once.Do(func() {
 			fmt.Fprintf(w, "Showing logs for %s job #%d.\n", job.Name, job.ID)
