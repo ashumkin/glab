0391b7cb280f2ac5e39902240d2c86c962f24045
Alexey Shumkin <Alex.Crezoff@gmail.com>
2024-03-24 16:03:30 +0300

    feat(ci view): respect terminal style

--- internal/commands/ci/view/view.go
+++ internal/commands/ci/view/view.go
@@ -345,6 +345,8 @@ func (o *options) run(ctx context.Context, args []string) error {
 		SetBackgroundColor(tcell.ColorDefault).
 		SetBorderPadding(1, 1, 2, 2).
 		SetBorder(true).
+		SetBorderStyle(tcell.StyleDefault).
+		SetTitleColor(tcell.ColorDefault).
 		SetTitle(fmt.Sprintf(" Pipeline #%d (%s) triggered %s by %s ", pipelineID, projectID, utils.TimeToPrettyTimeAgo(pipelineCreatedAt), pipelineUser.Name))
 
 	boxes = make(map[string]*tview.TextView)
@@ -793,9 +795,12 @@ func jobsView(
 			tv := tview.NewTextView()
 			tv.
 				SetDynamicColors(true).
+				SetTextColor(tcell.ColorDefault).
 				SetBackgroundColor(tcell.ColorDefault).
 				SetBorderPadding(0, 0, 1, 1).
 				SetBorder(true).
+				SetBorderStyle(tcell.StyleDefault).
+				SetTitleColor(tcell.ColorDefault).
 				SetTitle(" " + curJob.Name + " ").
 				SetTitleAlign(tview.AlignLeft)
 
@@ -958,8 +963,11 @@ func box(root *tview.Pages, key string, x, y, w, h int) *tview.TextView {
 	if !ok {
 		b = tview.NewTextView()
 		b.
+			SetTextStyle(tcell.StyleDefault).
 			SetBackgroundColor(tcell.ColorDefault).
-			SetBorder(true)
+			SetBorder(true).
+			SetBorderStyle(tcell.StyleDefault).
+			SetTitleColor(tcell.ColorDefault)
 		boxes[key] = b
 	}
 	b.SetRect(x, y, w, h)
