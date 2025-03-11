fa329a838af6c19d3dcfc66f5e45cf602765533f
Alexey Shumkin <Alex.Crezoff@gmail.com>
2025-03-11 17:05:21 +0300

    fix(ci view): box color is still blue when job was cancelled during job log view

--- internal/commands/ci/view/view.go
+++ internal/commands/ci/view/view.go
@@ -940,6 +940,7 @@ func jobsView(
 			b.SetBorderColor(tcell.ColorGrey)
 			statChar = '■'
 		case "canceled":
+			b.SetBorderColor(tcell.ColorGrey)
 			statChar = 'Ø'
 		case "skipped":
 			statChar = '»'
