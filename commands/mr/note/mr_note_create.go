d053777f9db81d9b92126b5dac7544934dbbfd48
Alexey Shumkin <Alex.Crezoff@gmail.com>
2025-03-24 11:27:32 +0300

    feat(mr note): add "--thread" option to create a discussion thread

--- commands/mr/note/mr_note_create.go
+++ commands/mr/note/mr_note_create.go
@@ -50,35 +50,58 @@ func NewCmdNote(f *cmdutils.Factory) *cobra.Command {
 				return fmt.Errorf("aborted... Note has an empty message.")
 			}
 
+			isThread, _ := cmd.Flags().GetBool("thread")
+			if err != nil {
+				return err
+			}
 			uniqueNoteEnabled, _ := cmd.Flags().GetBool("unique")
 
 			if uniqueNoteEnabled {
-				opts := &gitlab.ListMergeRequestNotesOptions{}
-				notes, err := api.ListMRNotes(apiClient, repo.FullName(), mr.IID, opts)
+				notes, err := api.ListMRNotes(apiClient, repo.FullName(), mr.IID, &gitlab.ListMergeRequestNotesOptions{})
 				if err != nil {
 					return fmt.Errorf("running merge request note deduplication: %v", err)
 				}
 				for _, noteInfo := range notes {
 					if noteInfo.Body == body {
-						fmt.Fprintf(f.IO.StdOut, "%s#note_%d\n", mr.WebURL, noteInfo.ID)
+						printNoteURL(f, mr.WebURL, noteInfo.ID)
+
 						return nil
 					}
 				}
 			}
+			if isThread {
+				thread, err := api.StartMRThread(apiClient, repo.FullName(), mr.IID, &gitlab.CreateMergeRequestDiscussionOptions{
+					Body: &body,
+				})
+				if err != nil {
+					return err
+				}
+				if len(thread.Notes) == 0 {
+					return fmt.Errorf("no notes found for created discussion!? (%s)", thread.ID)
+				}
+				noteInfo := thread.Notes[0]
+				printNoteURL(f, mr.WebURL, noteInfo.ID)
 
+				return nil
+			}
 			noteInfo, err := api.CreateMRNote(apiClient, repo.FullName(), mr.IID, &gitlab.CreateMergeRequestNoteOptions{
 				Body: &body,
 			})
 			if err != nil {
 				return err
 			}
+			printNoteURL(f, mr.WebURL, noteInfo.ID)
 
-			fmt.Fprintf(f.IO.StdOut, "%s#note_%d\n", mr.WebURL, noteInfo.ID)
 			return nil
 		},
 	}
 
 	mrCreateNoteCmd.Flags().StringP("message", "m", "", "Comment or note message.")
 	mrCreateNoteCmd.Flags().Bool("unique", false, "Don't create a comment or note if it already exists.")
+	mrCreateNoteCmd.Flags().BoolP("thread", "T", false, "Add a discussion thread rather than a comment")
 	return mrCreateNoteCmd
+}
+
+func printNoteURL(f *cmdutils.Factory, webURL string, noteID int) {
+	fmt.Fprintf(f.IO.StdOut, "%s#note_%d\n", webURL, noteID)
 }
